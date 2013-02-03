aad632680b5af8d9913268a1c36cf8d95428ecdb
Jeffry Degrande <jeffry@jeffrydegrande.com>
2013-02-03 15:02:03 -0200

    project files and stuff

---
+++ Golden TriangleTests/Golden_TriangleTests.h
@@ -0,0 +1,13 @@
+//
+//  Golden_TriangleTests.h
+//  Golden TriangleTests
+//
+//  Created by Jeffry Degrande on 1/28/13.
+//  Copyright (c) 2013 Jeffry Degrande. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Golden_TriangleTests : SenTestCase
+
+@end
---
+++ Golden TriangleTests/Golden_TriangleTests.m
@@ -0,0 +1,32 @@
+//
+//  Golden_TriangleTests.m
+//  Golden TriangleTests
+//
+//  Created by Jeffry Degrande on 1/28/13.
+//  Copyright (c) 2013 Jeffry Degrande. All rights reserved.
+//
+
+#import "Golden_TriangleTests.h"
+
+@implementation Golden_TriangleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Golden TriangleTests");
+}
+
+@end

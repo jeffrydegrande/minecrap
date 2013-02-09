4ebf3fbb5b19b86c59e315cff3b5cb970d1bd94b
Jeffry Degrande <jeffry@jeffrydegrande.com>
2013-02-09 01:42:22 -0200

    fix colors so they link correctly

---
+++ Minecrap/Color.h
@@ -0,0 +1,36 @@
+//
+//  Colors.h
+//  Minecrap
+//
+//  Created by Jeffry Degrande on 2/2/13.
+//  Copyright (c) 2013 Jeffry Degrande. All rights reserved.
+//
+
+#ifndef Minecrap_colors_h
+#define Minecrap_colors_h
+
+#import <Foundation/Foundation.h>
+
+extern GLfloat blue[];
+extern GLfloat red[];
+extern GLfloat green[];
+extern GLfloat yellow[];
+extern GLfloat purple[];
+extern GLfloat orange[];
+extern GLfloat white[];
+extern GLfloat black[];
+extern GLfloat coal[];
+extern GLfloat stone[];
+extern GLfloat brown[];
+extern GLfloat dcoal[];
+extern GLfloat dstone[];
+extern GLfloat dbrown[];
+extern GLfloat dblue[];
+extern GLfloat dred[];
+extern GLfloat dgreen[];
+extern GLfloat dyellow[];
+extern GLfloat dpurple[];
+extern GLfloat dorange[];
+extern GLfloat skyblue[];
+
+#endif
--- Minecrap/Color.m
+++ Minecrap/Color.m
@@ -1,18 +1,16 @@
 //
-//  defs.h
+//  Color.m
 //  Minecrap
 //
-//  Created by Jeffry Degrande on 2/2/13.
+//  Created by Jeffry Degrande on 2/8/13.
 //  Copyright (c) 2013 Jeffry Degrande. All rights reserved.
 //
 
-#ifndef Minecrap_defs_h
-#define Minecrap_defs_h
 
-// COLORS
+#import "Color.h"
 
-GLfloat blue[]     = {0.0, 0.0, 1.0, 1.0};
-GLfloat red[]      = {1.0, 0.0, 0.0, 1.0};
+GLfloat blue[] = {0.0, 0.0, 1.0,1.0};
+GLfloat red[]  = {1.0, 0.0, 0.0, 1.0};
 GLfloat green[]    = {0.0, 1.0, 0.0, 1.0};
 GLfloat yellow[]   = {1.0, 1.0, 0.0, 1.0};
 GLfloat purple[]   = {1.0, 0.0, 1.0, 1.0};
@@ -22,7 +20,6 @@ GLfloat black[]    = {0.0, 0.0, 0.0, 1.0};
 GLfloat coal[]     = {0.15, 0.15, 0.15, 1.0};
 GLfloat stone[]    = {0.5, 0.5, 0.5, 1.0};
 GLfloat brown[]    = {102.0/255, 69.0/255, 35.0/255, 1.0};
-
 GLfloat dcoal[]    = {0.075, 0.075, 0.075, 1.0};
 GLfloat dstone[]   = {0.25, 0.25, 0.25, 1.0};
 GLfloat dbrown[]   = {102.0/510, 69.0/510, 35.0/510, 1.0};
@@ -33,8 +30,3 @@ GLfloat dyellow[]  = {0.5, 0.5, 0.0, 1.0};
 GLfloat dpurple[]  = {0.5, 0.0, 0.5, 1.0};
 GLfloat dorange[]  = {0.5, 0.32, 0.0, 1.0};
 GLfloat skyblue[]  = {0.52, 0.74, 0.84, 1.0};
-
-
-
-
-#endif

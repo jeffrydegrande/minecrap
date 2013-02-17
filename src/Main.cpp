400a7461b0e7630781ad38361eba947ab2929b5f
Jeffry Degrande <jeffry@jeffrydegrande.com>
2013-02-17 20:46:31 -0300

    persist CVars config

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,10 @@
 #include "World.h"
 #include <CVars/CVar.h>
 
+
+#define CONFIG_FILE "user.cfg"
+
+
 #ifdef _WIN32
 int PASCAL WinMain(HINSTANCE instance_in, HINSTANCE previous_instance, LPSTR command_line, int show_style)
 #else
@@ -10,8 +14,12 @@ int main(int argc, char **argv)
 {
 
     CVarUtils::CreateCVar("flying", false, "Player can fly?");
+    CVarUtils::Load(CONFIG_FILE);
 
 	Engine engine;
 	engine.run();
+
+    CVarUtils::Save(CONFIG_FILE);
+
 	return 0;
 }
